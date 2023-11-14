8204e3e35bfcd04a513ac0247496a784624595ae
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-11-14 13:37:07 +0530

    Add Book model with user relationship, fields, and methods
    Created a new model named `Book` with the following fields:
    - `user`: ForeignKey relationship with the User model.
    - `title`: CharField with a maximum length of 20 characters.
    - `author`: CharField with a maximum length of 20 characters.
    - `isbn`: CharField with a maximum length of 13 characters (unique).
    - `cover_image`: ImageField for uploading cover images to the images/  directory.
    - `genre`: CharField with a maximum length of 50 characters.

--- BookHub/books/models.py
+++ BookHub/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+    user = models.ForeignKey(User, related_name='books', on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    isbn = models.CharField(max_length=13, unique=True)
+    cover_image = models.ImageField(upload_to='images/')
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+    
+    def get_rating_values(self):
+        return self.review.values_list('rating', flat=True)
