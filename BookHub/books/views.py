071a9a4e1cd4cea8820957ad5eb1f360404afbd5
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-11-14 18:30:15 +0530

    Added review feature for books >> >> - Implemented URL configuration in books/urls.py for handling adding reviews. >> - Created a new form in books/forms.py for validating review submissions. >> - Designed the HTML template template  to display and handle the review submission form and template component

--- BookHub/books/views.py
+++ BookHub/books/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Book
+from .models import Book,Review
+from django.shortcuts import reverse
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ReviewForm
 
 # Create your views here.
 class books_listview(ListView):
@@ -12,3 +17,16 @@ class book_detailview(DetailView):
     model = Book
     template_name='books/detail_book.html'
     context_object_name = 'book'
+
+class reviewCreate(LoginRequiredMixin,View):
+    template_name = 'books/detail_book.html'
+    def post(self,request,bookId):
+        form=ReviewForm(request.POST)
+        if form.is_valid():
+            data = form.save(commit=False)  # Create a Review instance but don't save it yet
+            data.reviewedUser = request.user
+            data.book_id = bookId  # Assuming you have a 'book' field in your Review model
+            data.save()
+            return HttpResponseRedirect(reverse('detail_book', args=(bookId,)))
+        else:
+            return HttpResponseRedirect(reverse('detail_book', args=(bookId,)))
