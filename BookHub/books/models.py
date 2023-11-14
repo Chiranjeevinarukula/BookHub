768511166bc8c4f4ac6024645aec5c1d851312ed
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-11-14 18:05:52 +0530

    Add Review model for book reviews
    - Created a new model named Review with fields for the reviewed user, review text, creation timestamp, and association with a book.
    - Integrated the Review model with the admin site for easy management and monitoring.

--- BookHub/books/models.py
+++ BookHub/books/models.py
@@ -15,3 +15,13 @@ class Book(models.Model):
     
     def get_rating_values(self):
         return self.review.values_list('rating', flat=True)
+    
+
+class Review(models.Model):
+    reviewedUser = models.ForeignKey(User, related_name='review', on_delete=models.CASCADE)
+    review = models.TextField(max_length=250,blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book,related_name='review',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
