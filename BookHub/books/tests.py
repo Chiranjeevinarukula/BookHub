6d09abfc062d24806a61b70aa56845f62754dbe0
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-11-15 15:56:32 +0530

    feat: Add book editing functionality
    - Added URL for editing books in urls.py.
    - Implemented view logic for editing books in views.py.
    - Added a corresponding test case for editing books in tests.py.

--- BookHub/books/tests.py
+++ BookHub/books/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http import HttpResponseForbidden
+from .models import Book
 
-# Create your tests here.
+class UpdateBookTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.book = Book.objects.create(
+            user=self.user,
+            title='Test Title',
+            author='Test Author',
+            isbn='1234567890123',
+            cover_image='path/to/cover.jpg',
+            genre='Test Genre'
+        )
+
+    def test_update_book_view_authority(self):
+        self.client.login(username='testuser', password='testpassword')
+        updated_data = {
+            'title': 'Updated Title',
+            'author': 'Updated Author',
+            'isbn': '9876543210987',
+            'cover_image': 'path/to/updated_cover.jpg',
+            'genre': 'Updated Genre'
+        }
+        response = self.client.post(reverse('update_book', args=[self.book.id]), data=updated_data)
+        self.assertRedirects(response, reverse('get_books'))
+        updated_book = Book.objects.get(id=self.book.id)
+        self.assertEqual(updated_book.title, 'Updated Title')
+        self.assertEqual(updated_book.author, 'Updated Author')
+        self.assertEqual(updated_book.isbn, '9876543210987')
+        self.assertEqual(updated_book.genre, 'Updated Genre')
+
+    def test_update_book_view_no_authority(self):
+        other_user = User.objects.create_user(username='otheruser', password='otherpassword')
+        self.client.login(username='otheruser', password='otherpassword')
+        response = self.client.post(reverse('update_book', args=[self.book.id]), data={'title': 'Updated Title'})
+        self.assertIsInstance(response, HttpResponseForbidden)
+        updated_book = Book.objects.get(id=self.book.id)
+        self.assertNotEqual(updated_book.title, 'Updated Title')
