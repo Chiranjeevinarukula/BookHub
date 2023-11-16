1bad19675c2da95a565d276cbb78b2e1efd9c06e
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-11-16 16:59:01 +0530

    feat: Add user profile view with CSS and HTML
    **Description:**
    - Implemented a profileView in views.py to display user profiles.
    - Created the corresponding HTML file (profile.html) with styling in profile.css.
    - The view fetches user information based on the provided primary key (pk).
    - Added a new URL path (profile/<int:pk>/) to route to the profile view.
    - Ensured proper rendering of user profiles with associated styling

--- BookHub/userProfile/tests.py
+++ BookHub/userProfile/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ProfileViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+
+    def test_profile_view_returns_200(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('profile', kwargs={'pk': self.user.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_view_uses_correct_template(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('profile', kwargs={'pk': self.user.pk})
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'account/profile.html')
+
+    def test_profile_view_displays_user_data(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('profile', kwargs={'pk': self.user.pk})
+        response = self.client.get(url)
+        self.assertContains(response, self.user.username)
+        self.assertContains(response, self.user.email)
