27a78b4017f345f814d0548fa49e00d68454f24c
Chiranjeevinarukula <149758615+Chiranjeevinarukula@users.noreply.github.com>
2023-11-18 19:40:04 +0530

    Revert "Create login view template and define URL (#4)"
    This reverts commit 9696832c17dda63bb0d7281626e0de45f0e00552.

--- BookHub/BookHub/urls.py
+++ BookHub/BookHub/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
 ]
