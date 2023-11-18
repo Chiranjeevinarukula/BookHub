5cd6d8831d3736ac3ccb245ee1ef171661f9b89b
Chiranjeevinarukula <149758615+Chiranjeevinarukula@users.noreply.github.com>
2023-11-18 21:31:12 +0530

    feat : Create signup view , template and define URL (#5)
    Added the signup form to validate user inputs and configured the URL for the signup feature. The signup view template . includes the necessary components for capturing user information. The backend logic for handling signup requests will be implemented in subsequent commits.

--- BookHub/userProfile/views.py
+++ BookHub/userProfile/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import signUpForm
+from django.views import View
+from django.contrib.auth import login
+
 
 # Create your views here.
+
+class SignUpView(View):
+    def get(self, request):
+        form = signUpForm()
+        return render(request, 'account/signup.html', {'form': form})
+    def post(self, request):
+        form = signUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('get_books')  # Redirect to the desired URL upon successful signup
+        return render(request, 'account/signup.html', { 'form': form })
